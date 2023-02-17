25acf0901208dd0c62caf1bb01f620f96daff039
maxbond <ceo@rf.ru>
2023-02-17 23:34:02 +0300

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Logo, Header} from './styles';
+
+function HeaderComponent() {
+  return (
+    <div>
+      <Logo title='Milky Mood' />
+    </div>
+  );
+}
+
+export default HeaderComponent;
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        color: #FBFBFB;
+        height: 100vh;
+        width: 100vw;
+        padding: 0;
+        margin: 0;
+        overflow: hidden;
+        text-rendering: antialiased;
+        background-color: #2828e6;
+        font-family: 'Work Sans', sans-serif;
+        font-weight: 500;
+        
+    }
+`;
